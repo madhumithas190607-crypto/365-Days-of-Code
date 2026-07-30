2b00f59c4e3fee77b2a08b4d4d55952abcb5bede
Madhumitha S <madhumithas190607@gmail.com>
2026-07-30 19:42:45 +0530

    Day 002: Variables in Java

---
+++ Day-002-Variables/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        int age = 20;
+        double cgpa = 8.5;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name = "Madhumitha";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("CGPA: " + cgpa);
+        System.out.println("Grade: " + grade);
+        System.out.println("Student: " + isStudent);
+    }
+}
