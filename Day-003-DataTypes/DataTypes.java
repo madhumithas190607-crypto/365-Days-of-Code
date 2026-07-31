2eab8023b9c4485e298c5680b83ee482ce044f33
Madhumitha S <madhumithas190607@gmail.com>
2026-07-31 21:36:44 +0530

    Day 003: Data Types in Java

---
+++ Day-003-DataTypes/DataTypes.java
@@ -0,0 +1,27 @@
+public class DataTypes {
+    public static void main(String[] args) {
+
+        byte age = 20;
+        short year = 2026;
+        int salary = 30000;
+        long population = 8000000000L;
+
+        float height = 5.8f;
+        double weight = 60.5;
+
+        char grade = 'A';
+        boolean isPlaced = false;
+
+        String name = "Madhumitha";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Year: " + year);
+        System.out.println("Salary: " + salary);
+        System.out.println("Population: " + population);
+        System.out.println("Height: " + height);
+        System.out.println("Weight: " + weight);
+        System.out.println("Grade: " + grade);
+        System.out.println("Placed: " + isPlaced);
+    }
+}
