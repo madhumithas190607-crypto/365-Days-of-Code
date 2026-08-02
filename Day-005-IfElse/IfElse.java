b9786d973ec0326e77398140808993bbc73b27c1
Madhumitha S <madhumithas190607@gmail.com>
2026-08-02 20:19:17 +0530

    Day 004: Operators in Java

---
+++ Day-005-IfElse/IfElse.java
@@ -0,0 +1,14 @@
+public class IfElse {
+    public static void main(String[] args) {
+
+        int marks = 75;
+
+        System.out.println("Student Marks: " + marks);
+
+        if (marks >= 50) {
+            System.out.println("Result: Pass");
+        } else {
+            System.out.println("Result: Fail");
+        }
+    }
+}
