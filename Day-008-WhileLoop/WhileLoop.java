244c0dc2c4ae9b5abbd70a95ba088f21ffc13016
Madhumitha S <madhumithas190607@gmail.com>
2026-08-05 14:10:00 +0530

    Day 007: While Loop in Java

---
+++ Day-008-WhileLoop/WhileLoop.java
@@ -0,0 +1,13 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        System.out.println("Numbers from 1 to 10:");
+
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
