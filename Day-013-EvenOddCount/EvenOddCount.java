8d8778c9a5236239f6c9296390e15539c2d79523
Madhumitha S <madhumithas190607@gmail.com>
2026-08-23 10:30:06 +0530

    Day 013: Count Even and Odd Numbers

---
+++ Day-013-EvenOddCount/EvenOddCount.java
@@ -0,0 +1,21 @@
+public class EvenOddCount {
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 15, 20, 25, 30, 35};
+
+        int evenCount = 0;
+        int oddCount = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+
+            if (numbers[i] % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
+
+        System.out.println("Even numbers count: " + evenCount);
+        System.out.println("Odd numbers count: " + oddCount);
+    }
+}
