9893bcb65291d1862eb28e3a873532676d4acf77
Madhumitha S <madhumithas190607@gmail.com>
2026-08-24 18:27:53 +0530

    Day 014: Methods in Java

---
+++ Day-014-Methods/Methods.java
@@ -0,0 +1,20 @@
+public class Methods {
+
+    static void greet() {
+        System.out.println("Hello, welcome to Day 14!");
+    }
+
+    static void addNumbers() {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+
+        System.out.println("Sum: " + sum);
+    }
+
+    public static void main(String[] args) {
+
+        greet();
+        addNumbers();
+    }
+}
