3bd7bb1f107ba4d726d449ca69731f43620a9421
Madhumitha S <madhumithas190607@gmail.com>
2026-08-27 22:07:17 +0530

    Day 017: Strings in Java

---
+++ Day-017-Strings/Strings.java
@@ -0,0 +1,12 @@
+public class Strings {
+    public static void main(String[] args) {
+
+        String name = "Madhumitha";
+
+        System.out.println("Name: " + name);
+        System.out.println("Length: " + name.length());
+        System.out.println("Uppercase: " + name.toUpperCase());
+        System.out.println("Lowercase: " + name.toLowerCase());
+        System.out.println("First Character: " + name.charAt(0));
+    }
+}
