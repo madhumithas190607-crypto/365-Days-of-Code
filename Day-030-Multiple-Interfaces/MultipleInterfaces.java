86dfb32b9640c7a69e3a0169881f55e9dfaaf081
Madhumitha S <madhumithas190607@gmail.com>
2026-09-10 21:59:55 +0530

    Day 030: Multiple Interfaces in Java

---
+++ Day-030-Multiple-Interfaces/MultipleInterfaces.java
@@ -0,0 +1,31 @@
+interface Animal {
+
+    void sound();
+}
+
+interface Pet {
+
+    void play();
+}
+
+class Dog implements Animal, Pet {
+
+    public void sound() {
+        System.out.println("Dog barks");
+    }
+
+    public void play() {
+        System.out.println("Dog plays");
+    }
+}
+
+public class MultipleInterfaces {
+
+    public static void main(String[] args) {
+
+        Dog dog = new Dog();
+
+        dog.sound();
+        dog.play();
+    }
+}
